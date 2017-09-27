6b91219837d465dc536605c2fb6b3ffdd5eb2f2e
huy <htran77@csc.com>
2017-09-27 15:57:51 +0700

    commit

---
+++ src/Test.java
@@ -0,0 +1,45 @@
+import java.util.Properties;  
+import javax.mail.*;  
+import javax.mail.internet.*;  
+  
+public class Test {  
+ public static void main(String[] args) {  
+  
+  String host="smtp.gmail.com";  
+  final String user="dinhhuy0610@gmail.com";//change accordingly  
+  final String password="01663036577";//change accordingly  
+    
+  String to="huyt71@gmail.com";//change accordingly  
+  
+   //Get the session object  
+   Properties props = new Properties();  
+   props.put("mail.smtp.host", "smtp.gmail.com");
+	props.put("mail.smtp.socketFactory.port", "465");
+	props.put("mail.smtp.socketFactory.class",
+			"javax.net.ssl.SSLSocketFactory");
+	props.put("mail.smtp.auth", "true");
+	props.put("mail.smtp.port", "465");
+     
+   Session session = Session.getDefaultInstance(props,  
+    new javax.mail.Authenticator() {  
+      protected PasswordAuthentication getPasswordAuthentication() {  
+    return new PasswordAuthentication(user,password);  
+      }  
+    });  
+  
+   //Compose the message  
+    try {  
+     MimeMessage message = new MimeMessage(session);  
+     message.setFrom(new InternetAddress(user));  
+     message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));  
+     message.setSubject("javatpoint");  
+     message.setText("This is simple program of sending email using JavaMail API");  
+       
+    //send the message  
+     Transport.send(message);  
+  
+     System.out.println("message sent successfully...");  
+   
+     } catch (MessagingException e) {e.printStackTrace();}  
+ }  
+}  
